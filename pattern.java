a0fe1115b520b85df8622f0cf43c025c72e17761
ReneeR79 <renee.rosenheim@gmail.com>
2025-11-06 16:04:05 +0000

    start

--- pattern.java
+++ pattern.java
@@ -1,1 +1,40 @@
+public class Pattern{
+Create a method with the signature:
+public static void stars(int rows){}
+
+//Uses  while loops to print out the following pattern (shown with 7 rows):
+
+
+
+//Create a method with the signature:
+public static void triangle(int rows){}
+
+
+//Uses  do … while or while loops to print out the following pattern (shown here with 9 rows):
+
+
+//Create a method with the signature:
+public static void odds(int start){}
+
+
+//Use  for loops to print out the following pattern (shown here with start being 9):
+
+
+
+//Create a method with the signature:
+public static void eo(int maxE){}
+
+//Uses  loops of your choice to print out the following pattern (shown here with maxE equal to 6):
+
+
+//Please note that if given an odd input you’ll need to start the first row with an “E” instead of an “O”, for example maxE = 5
+
+
+
+//Create a method with the signature:
+public static void pyramid(int rows){}
+
+//Using loops of your choice print the pattern below (shown here with five rows):
+
+}
 
